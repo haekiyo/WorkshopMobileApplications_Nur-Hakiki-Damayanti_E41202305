b3094d4531c647cffb691a8f4c4b3178fa6690d1
Nur Hakiki Damayanti <dmyntkk07@gmail.com>
2021-11-23 01:45:01 +0700

    RetroVolley

---
+++ RetroVolley/koneksi/retrofit/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$db   = "user1";
+
+$conn = mysqli_connect($host, $user, $password, $db) or die("Gagal menghubungi server!");;
+
+?>
